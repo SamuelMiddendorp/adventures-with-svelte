c684fb3f7475358b5318f527036385f1decf3e55
Samuel Middendorp <samiezaurus@gmail.com>
2023-03-14 18:34:08 +0100

    Simple counter

---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let counter: number = 0;
+    $: {
+        console.log(counter);
+    } 
+</script>
+<body>
+    <button on:click={() => counter += 1}>count: {counter}</button>
+</body>
