baecb54c79d4191e7ff4be895bffad3b421f8214
Samuel Middendorp <samiezaurus@gmail.com>
2023-03-16 23:00:17 +0100

    Self deleting cards with component communication

---
+++ src/components/selfDeletingCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let data: String;
+    export let deleteCard = (data: String) => {}
+</script>
+<style>
+    .card{
+        padding: 1em;
+        border-radius: 0.2em;
+        border: 1px solid #222;
+        width: max-content;
+    }
+</style>
+<body>
+    <div class="card">{data}
+    <button on:click={() => deleteCard(data)}>remove from list</button>
+    </div>
+</body>
---
+++ src/routes/selfDeletingCards/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import SelfDeletingCard from "../../components/selfDeletingCard.svelte";
+
+    let cards : String[] = ["Foo", "Bar", "Bas", "BasBar", "BarBas"];
+    function deleteCard(card: String){
+        cards = cards.filter(x => x != card);
+    }
+</script>
+<style>
+</style>
+<body>
+    {#each cards as card}
+        <SelfDeletingCard  data={card} deleteCard={deleteCard}/>
+    {/each}
+</body>
