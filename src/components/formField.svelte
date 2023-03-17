24717d2a80e14679ccb4116a954d914b27fc359d
Samuel Middendorp <samiezaurus@gmail.com>
2023-03-17 23:03:54 +0100

    Added field component with some upwards communication

---
+++ src/components/formField.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { validate_each_argument } from "svelte/internal";
+
+
+    interface FieldProps{
+        name: string,
+        label: string,
+        type: string,
+        onValue: (value: string) => void
+    }
+    function update(e: Event){
+        let event = e.target as HTMLInputElement
+        props.onValue(event.value)
+    }
+    export let props: FieldProps; 
+</script>
+<body>
+    <label for={props.name}>{props.label}</label>
+    <input type={props.type} name={props.name} on:input={update} />
+</body>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-    import {fooData} from "../stores/sampleStore";
+    import FormField from "../components/formField.svelte";
+import {fooData} from "../stores/sampleStore";
     let word: string = "";
+    function handle(e: string){
+        console.log(e);
+    }
 </script>
 <body>
     <h2>Some data:</h2>
@@ -8,5 +12,6 @@
         <p>{foo}</p>
     {/each}
     <input bind:value={word}> 
+    <FormField props={{name: "foo", type: "password", label: "foo:", onValue: handle}}/>
     <button on:click={() => fooData.set([...$fooData, word])}>Add to store</button>
 </body>
