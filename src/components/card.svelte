ec454f8a72ebc23a31cbfb295be383b4eaba2eeb
Samuel Middendorp <samiezaurus@gmail.com>
2023-03-19 21:01:18 +0100

    Added cards with a grid layout

---
+++ src/components/card.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    interface CardProps{
+        name: string,
+        description: string,
+        price: string,
+    }
+    export let props: CardProps; 
+</script>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200&display=swap');
+    .card
+    {
+        padding: 1em;
+        font-family: 'Josefin Sans', sans-serif;
+        display: grid;
+        border: 1px solid #333;
+    }
+</style>
+<div class="card">
+    <h2>{props.name}</h2>
+    <p>{props.description}</p>
+    <h2>{props.price}</h2>
+</div>
---
+++ src/routes/components/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import Card from "../../components/card.svelte";
+
+
+
+</script>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .con{
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(150px, max-content));
+    }
+</style>
+<body>
+    <div class="con">
+    {#each {length: 10} as _,i}
+        <Card props={{name: "Sample", description: "A description about this sample", price: "120"}}/>
+    {/each}
+    </div>
+</body>
